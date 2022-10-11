a0f155efb76607bac5e3f6ed524be038e78e5725
Alperen Onal <alperenonal9922@gmail.com>
2022-10-11 21:43:29 +0300

    adjustments

--- JavaLessons/src/main/java/com/mycompany/javalessons/SecondWeek/Inheritance/Credits/BaseCreditManager.java
+++ JavaLessons/src/main/java/com/mycompany/javalessons/SecondWeek/Inheritance/Credits/BaseCreditManager.java
@@ -3,6 +3,7 @@ package com.mycompany.javalessons.SecondWeek.Inheritance.Credits;
 public class BaseCreditManager {
     
     public void getResult(){
+        // Algorithm codes here...
         System.out.println("result = ...");
     }
 }
